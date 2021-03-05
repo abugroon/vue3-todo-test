6b66af5128f48ed87cb4379c07de4257f3022e1f
abugroon <moawiaabugroon@gmail.com>
2021-03-06 00:41:07 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <ul class="nav w-100 bg-dark justify-content-center">
+      <li class="nav-item">
+        <router-link active-class="active" class="nav-link" to="/"
+          >Home</router-link
+        >
+      </li>
+      <li class="nav-item">
+        <router-link active-class="active" class="nav-link" to="/about"
+          >About</router-link
+        >
+      </li>
+    </ul>
+  <div class="container mt-4">
+    <div class="row">
+      <router-view />
+    </div>
+  </div>
+</template>
+<style lang="scss">
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="container">
+    <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+    <h1 class="text-primary">Todo List</h1>
+    <div class="row mt-2">
+      <div
+        class="col-md-6 col-sm-12 mb-2 col-lg-4 d-flex align-items-stretch "
+        :key="todo.id"
+        v-for="(todo, index) in todos.splice(0, 12)"
+      >
+        <div class="card w-100 ">
+          <div class="card-body">
+            <h5 class="card-text">{{ index + 1 }} / {{ todo.title }}</h5>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+import { ref } from "vue";
+export default {
+  setup() {
+    let todos = ref([]);
+    const loadData = () => {
+      fetch("https://jsonplaceholder.typicode.com/todos")
+        .then((response) => response.json())
+        .then((json) => (todos.value = json));
+    };
+    loadData();
+    return {
+      HelloWorld,
+      todos,
+    };
+  },
+};
+</script>
